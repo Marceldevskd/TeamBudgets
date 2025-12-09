752a6b83baf8ae5ea016f7b6db86ce234ee40a89
marceldevskd <marceldevisser2006@gmail.com>
2025-12-09 17:15:52 +0100

    change db setup

--- database/migrations/002_add_budgets.sql
+++
@@ -1,9 +0,0 @@
-
-
-CREATE TABLE IF NOT EXISTS budgets (
-    id SERIAL PRIMARY KEY,
-    team SERIAL REFERENCES teams(id),
-    amount DECIMAL(15, 2) NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL
-);
---
+++ database/schemas/init.sql
@@ -0,0 +1,20 @@
+
+
+CREATE TABLE IF NOT EXISTS teams (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS people (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    team SERIAL REFERENCES teams(id)
+);
+
+CREATE TABLE IF NOT EXISTS budgets (
+    id SERIAL PRIMARY KEY,
+    team SERIAL REFERENCES teams(id),
+    amount DECIMAL(15, 2) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL
+);
