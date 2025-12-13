f7b518b78633608cc82da1269bd4474ab29b7d44
marceldevskd <marceldevisser2006@gmail.com>
2025-12-13 14:48:22 +0100

    fix budget controllers check if exists

--- src/controllers/budget/index.ts
+++ src/controllers/budget/index.ts
@@ -1,21 +1,21 @@
 import { GetBudgetsRequest, GetBudgetsResponse, CreateBudgetsRequest, CreateBudgetsResponse, ChangeBudgetsRequest, ChangeBudgetsResponse } from "../../routes/budget/types";
 import { getBudgetsService } from "../../services/budget";
-import { team_id_exists, team_name_exists, person_name_exists } from "../../helpers/exists";
+import { checkIfExists } from "../../helpers/exists";
 
 export const getBudgetsController = async (req: GetBudgetsRequest, res: GetBudgetsResponse) => {
     try {
         if (req.query.team_id !== undefined) {
-            if (!await team_id_exists(req.query.team_id as string)) {
+            if (!await checkIfExists("teams", "id", req.query.team_id as string)) {
                return res.status(400).send({error: "Team ID does not exist"});
             }
         }
         if (req.query.team_name !== undefined) {
-            if (!await team_name_exists(req.query.team_name as string)) {
+            if (!await checkIfExists("teams", "name", req.query.team_name as string)) {
                 return res.status(400).send({error: "Team name does not exist"}); 
             }
         }
         if (req.query.person_name !== undefined) {
-            if (!await person_name_exists(req.query.person_name as string)) {
+            if (!await checkIfExists("people", "name", req.query.person_name as string)) {
                 return res.status(400).send({error: "Person name does not exist"}); 
             }
         }
--- src/helpers/exists.ts
+++ src/helpers/exists.ts
@@ -1,29 +1,5 @@
 import pool from "../database";
 
-export const team_id_exists = async (team_id: string): Promise<boolean> => {
-    const result = await pool.query("SELECT id FROM teams WHERE id = $1", [team_id]);
-    if (result.rowCount === 0 || !result) {
-        return false;
-    }
-    return true;
-}
-
-export const team_name_exists = async (team_name: string): Promise<boolean> => {
-    const result = await pool.query("SELECT id FROM teams WHERE name = $1", [team_name]);
-    if (result.rowCount === 0 || !result) {
-        return false;
-    }
-    return true;
-}
-
-export const person_name_exists = async (person_name: string): Promise<boolean> => {
-    const result = await pool.query("SELECT id FROM people WHERE name = $1", [person_name]);
-    if (result.rowCount === 0 || !result) {
-        return false;
-    }
-    return true;
-}
-
 export const checkIfExists = async (table: string, column: string, value: string): Promise<boolean> => {
     const query = `SELECT id FROM ${table} WHERE ${column} = $1`;
     const result = await pool.query(query, [value]);
